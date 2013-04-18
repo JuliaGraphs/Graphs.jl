6df17e39974f20a2460ff203a6316381d01f983f
Dahua Lin <lindahua@gmail.com>
2013-04-18 14:05:45 -0500

    add test_all

---
+++ test/test_all.jl
@@ -0,0 +1,13 @@
+# graph classes
+
+include("adjlist.jl")
+include("inclist.jl")
+
+# algorithms
+
+include("bfs.jl")
+include("dfs.jl")
+include("conn_comp.jl")
+include("dijkstra.jl")
+include("mst.jl")
+include("floyd.jl")
