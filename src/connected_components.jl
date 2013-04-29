c75af51954df97fac7641b0465bf68743f023c2d
Max Likely <maxlikely+login@gmail.com>
2013-04-29 18:05:28 -0400

    Adds recursive Tarjans.

--- src/connected_components.jl
+++ src/connected_components.jl
@@ -27,4 +27,60 @@ function connected_components{V}(graph::AbstractGraph{V})
     components
 end
 
-## TODO: add a function to detect strongly connected components of directed graph
+###########################################################
+#
+#   Connected components of directed graph
+#
+##########################################################
+
+function strongly_connected_components{V}(graph::AbstractGraph{V})
+    @graph_requires graph vertex_list vertex_map adjacency_list
+
+    if !is_directed(graph)
+        throw(ArgumentError("graph must be directed."))
+    end
+
+    idx = v -> vertex_index(v, graph)
+    index = 1
+    stack = Array(V, 0)
+    lowlinks = zeros(Int, num_vertices(graph))
+    indices  = zeros(Int, num_vertices(graph))
+    components = Array(Vector{V}, 0)
+
+    function strongconnect(v)
+        indices[idx(v)]  = index
+        lowlinks[idx(v)] = index
+        index += 1
+        push!(stack, v)
+
+        # consider successors of v
+        for w in out_neighbors(v, graph)
+            if indices[idx(w)] == 0 # w is un-visited
+                strongconnect(w)
+                lowlinks[idx(v)] = min(lowlinks[idx(v)], lowlinks[idx(w)])
+            elseif contains(stack, w)
+                lowlinks[idx(v)] = min(lowlinks[idx(v)], indices[idx(w)])
+            end
+        end
+
+        # if v is a root node, pop the stack and generate an SCC
+        if lowlinks[idx(v)] == indices[idx(v)]
+            component = Array(typeof(v), 0)
+            while !isempty(stack)
+                w = pop!(stack)
+                push!(component, w)
+                if w == v
+                    break
+                end
+            end
+            push!(components, component)
+        end 
+    end
+
+    for v in vertices(graph)
+        if indices[idx(v)] == 0
+            strongconnect(v)
+        end
+    end
+    components
+end
