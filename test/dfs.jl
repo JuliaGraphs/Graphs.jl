e601b20c70fb82ccc2e76273bc6a4d12f3856806
Matt Weiden <weiden@soundcloud.com>
2013-07-20 02:27:49 +0200

    Fix cyclic test for graps with ExVert vertices.
    The test_cyclic_by_dfs method previously assumed that the graph being passed
    in had vertices of type int. This caused an exception to be thrown when trying
    to run test_cyclic_by_dfs on graphs with ExVert vertices.
    
    This commit resolves the issue.

--- test/dfs.jl
+++ test/dfs.jl
@@ -3,26 +3,25 @@
 using Graphs
 using Base.Test
 
-g = simple_adjlist(6)
+acyclic = [(1,2) (1,3) (1,6) (2,4) (2,5) (3,5) (3,6)]
+cyclic  = [(1,2) (1,3) (1,6) (2,4) (2,5) (3,5) (3,6) (5,1)]
 
-add_edge!(g, 1, 2)
-add_edge!(g, 1, 3)
-add_edge!(g, 1, 6)
-add_edge!(g, 2, 4)
-add_edge!(g, 2, 5)
-add_edge!(g, 3, 5)
-add_edge!(g, 3, 6)
+g = simple_adjlist(6)
+map((edg) -> add_edge!(g, edg[1], edg[2]), acyclic)
 
 g2 = simple_adjlist(6)
+map((edg) -> add_edge!(g2, edg[1], edg[2]), cyclic)
 
-add_edge!(g2, 1, 2)
-add_edge!(g2, 1, 3)
-add_edge!(g2, 1, 6)
-add_edge!(g2, 2, 4)
-add_edge!(g2, 2, 5)
-add_edge!(g2, 3, 5)
-add_edge!(g2, 3, 6)
-add_edge!(g2, 5, 1)
+
+gEx = graph(ExVertex, ExEdge{ExVertex}, is_directed = true)
+map((x) -> add_vertex!(gEx, "edge:" * string(x)), 1:6)
+V = vertices(gEx)
+map((edg) -> add_edge!(gEx, V[edg[1]], V[edg[2]]), acyclic)
+
+gEx2 = graph(ExVertex, ExEdge{ExVertex}, is_directed = true)
+map((x) -> add_vertex!(gEx2, "edge:" * string(x)), 1:6)
+V2 = vertices(gEx2)
+map((edg) -> add_edge!(gEx2, V2[edg[1]], V2[edg[2]]), cyclic)
 
 # DFS traversal
 
@@ -36,6 +35,11 @@ vs2 = visited_vertices(g2, DepthFirst(), 1)
 
 @assert test_cyclic_by_dfs(g) == false
 @assert test_cyclic_by_dfs(g2) == true
+
+# Cyclic test with Extended Graph types
+
+@assert test_cyclic_by_dfs(gEx) == false
+@assert test_cyclic_by_dfs(gEx2) == true
 
 # Topological sort
 
