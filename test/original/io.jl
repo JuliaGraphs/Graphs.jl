29eacd5f391c0ae4448b39525a4933fc16ad38a4
Dahua Lin <lindahua@gmail.com>
2014-02-17 22:32:18 -0600

    remove stuff that was obsoleted long ago

--- test/original/io.jl
+++
@@ -1,3 +0,0 @@
-pathname = joinpath("test", "data", "graph1.edgelist")
-g = read_edgelist(pathname)
-@assert isequal(adjacency_matrix(g), [0 1 1; 0 0 1; 0 0 0;])
--- test/original/vertex.jl
+++
@@ -1,7 +0,0 @@
-v1 = Vertex(1, "A")
-v2 = Vertex(2, "B")
-
-Set(v1, v2)
-
-@assert isequal(id(v1), 1)
-@assert isequal(label(v1), "A")
