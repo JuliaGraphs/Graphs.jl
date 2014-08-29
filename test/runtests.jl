987d8788c7dd18100977ebd4008aa304d6fbeffe
Iain Dunning <iaindunning@gmail.com>
2014-08-28 22:21:49 -0400

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,24 @@
+tests = [
+    "edgelist",
+    "adjlist",
+    "bellman_test",
+    "inclist",
+    "graph",
+    "gmatrix",
+    "bfs",
+    "dfs",
+    "conn_comp",
+    "dijkstra",
+    "a_star_spath",
+    "mst",
+    "floyd",
+    "dot",
+    "cliques",
+    "random" ]
+
+
+for t in tests
+    tp = joinpath(Pkg.dir("Graphs"),"test","$(t).jl")
+    println("running $(tp) ...")
+    include(tp)
+end
