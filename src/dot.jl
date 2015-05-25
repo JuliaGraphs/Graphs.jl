6a3e4f93169b75618e7156eeaca1465264e29165
Alain Lichnewsky <Alain.Lichnewsky@gmail.com>
2015-05-25 23:52:47 +0200

    Add capability to show node labels when producing .dot graphical representation

--- src/dot.jl
+++ src/dot.jl
@@ -25,6 +25,9 @@ function to_dot{G<:AbstractGraph}(graph::G, stream::IO,attrs::AttributeDict=Attr
     write(stream, "$(graph_type_string(graph)) graphname {\n")
     write(stream, "$(to_dot_graph(attrs))")
     if implements_edge_list(graph) && implements_vertex_map(graph)
+        for vtx in  vertices(graph)
+            write(stream,"$(vertex_index(vtx,graph))\t$(to_dot(attributes(vtx,graph)))\n")
+        end
         for edge in edges(graph)
             write(stream,"$(vertex_index(source(edge), graph)) $(edge_op(graph)) $(vertex_index(target(edge), graph))\n")
         end
