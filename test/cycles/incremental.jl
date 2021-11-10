6d4bfd1da4396c5754f387db322917172e1087fb
Keno Fischer <keno@juliacomputing.com>
2021-11-10 02:35:05 -0500

    Add incremental cycle detection algorithm
    This adds the abstract interface for and a basic, naive implementation
    of an algorithm to solve the incremental (online) cycle detection problem.
    The incremental cycle detection problem is as follows:
    
    Starting from some initial acyclic (directed) graph (here taken to be empty),
    process a series of edge additions, stopping after the first edge
    addition that introduces a cycle.
    
    The algorithms for this problem have been developed and improved mostly
    over the past five years or so, so they are not currently widely used
    (or available in other libraries). That said, ModelingToolkit was using
    an implementation in its DAE tearing code (which I intend to replace
    with a version based on this code) and there are recent papers showing
    applications in compiler optimizations.
    
    The main focus of the current PR is the abstract interface. The algorithm
    itself is Algorithm N (for "Naive") from [BFGT15] Section 3. The reference
    develops several more algorithms with differing performance patterns,
    depending on the sparsity of the graph and there are further improvements
    to the asymptotics in the subsequent literature. However, Algorithm N is
    simple to understand and extend and works ok for what is needed in MTK,
    so I am not currently planning to implement the more advanced algorithms.
    I do want to make sure the extension point exists to add them in the future,
    which I believe this approach accomplishes.
    
    [BFGT15] Michael A. Bender, Jeremy T. Fineman, Seth Gilbert, and Robert E. Tarjan. 2015
        A New Approach to Incremental Cycle Detection and Related Problems.
        ACM Trans. Algorithms 12, 2, Article 14 (December 2015), 22 pages.
        DOI: http://dx.doi.org/10.1145/2756553

---
+++ test/cycles/incremental.jl
@@ -0,0 +1,38 @@
+@testset "ICT" begin
+    Gempty = SimpleDiGraph(3)
+    Gsomedges = SimpleDiGraph(6)
+    add_edge!(Gsomedges, 4, 5)
+    add_edge!(Gsomedges, 6, 5)
+    for Gtemplate in (Gempty, Gsomedges)
+        for dir in (:out, :in)
+            G = copy(Gtemplate)
+            ict = IncrementalCycleTracker(G; dir)
+            @test_nowarn repr(ict)
+            @test add_edge_checked!(ict, 1, 2)
+            @test add_edge_checked!(ict, 2, 3)
+            @test length(edges(G)) == 2 + length(edges(Gtemplate))
+            @test !add_edge_checked!(ict, 3, 1)
+            @test !add_edge_checked!(ict, 3, 2)
+            if dir === :in
+                @test !add_edge_checked!(ict, (2, 3), 1)
+                @test !add_edge_checked!(ict, (1, 3), 2)
+            else
+                @test !add_edge_checked!(ict, 3, (1,2))
+                @test !add_edge_checked!(ict, 2, (1,3))
+            end
+            @test length(edges(G)) == 2 + length(edges(Gtemplate))
+            @test filter(in((1,2,3)), topological_sort(ict)) == [1, 2, 3]
+        end
+    end
+
+    Gcycle2 = SimpleDiGraph(2)
+    add_edge!(Gcycle2, 1, 2)
+    add_edge!(Gcycle2, 2, 1)
+    @test_throws ErrorException IncrementalCycleTracker(Gcycle2)
+
+    Gcycle3 = SimpleDiGraph(3)
+    add_edge!(Gcycle3, 1, 2)
+    add_edge!(Gcycle3, 2, 3)
+    add_edge!(Gcycle3, 3, 1)
+    @test_throws ErrorException IncrementalCycleTracker(Gcycle3)
+end
