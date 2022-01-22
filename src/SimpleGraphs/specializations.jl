2838b3f49f55e0569c20c7c22b6817ccaee0f5cb
Simon Schoelly <sischoel@gmail.com>
2022-01-22 18:51:10 +0100

    Add an always copy option for squashing simple graphs

--- src/SimpleGraphs/specializations.jl
+++ src/SimpleGraphs/specializations.jl
@@ -1,22 +1,29 @@
 
-function Graphs.squash(g::Union{SimpleGraph, SimpleDiGraph})
+"""
+    squash(g::Union{SimpleGraph. SimpleDiGraph}; alwayscopy=true)
+
+Specialised version of `Graphs::Squash` for `SimpleGraph` and `SimpleDiGraph`.
+If `alwayscopy` is `true`, the resulting graph will always be a copy, otherwise
+it can also be the original graph.
+"""
+function Graphs.squash(g::Union{SimpleGraph, SimpleDiGraph}; alwayscopy::Bool=true)
 
     G = is_directed(g) ? SimpleDiGraph : SimpleGraph
     T = eltype(g)
 
-    T <: Union{Int8, UInt8}   && return g
+    (!alwayscopy && T <: Union{Int8, UInt8}) && return g
     nv(g) < typemax(Int8)     && return G{Int8}(g)
     nv(g) < typemax(UInt8)    && return G{UInt8}(g)
-    T <: Union{Int16, UInt16} && return g
+    (!alwayscopy && T <: Union{Int16, UInt16}) && return g
     nv(g) < typemax(Int16)    && return G{Int16}(g)
     nv(g) < typemax(UInt16)   && return G{UInt16}(g)
-    T <: Union{Int32, UInt32} && return g
+    (!alwayscopy && T <: Union{Int32, UInt32}) && return g
     nv(g) < typemax(Int32)    && return G{Int32}(g)
     nv(g) < typemax(UInt32)   && return G{UInt32}(g)
-    T <: Union{Int64, UInt64} && return g
+    (!alwayscopy && T <: Union{Int64, UInt64}) && return g
     nv(g) < typemax(Int64)    && return G{Int64}(g)
     nv(g) < typemax(UInt64)   && return G{UInt64}(g)
 
-    return g
+    return alwayscopy ? copy(g) : g
 end
 
