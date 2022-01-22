03d6114f8f6fec956e8e259a9949402d6f13e03b
Simon Schölly <sischoel@gmail.com>
2022-01-23 00:28:49 +0100

    Update src/SimpleGraphs/specializations.jl
    Co-authored-by: Mathieu Besançon <mathieu.besancon@gmail.com>

--- src/SimpleGraphs/specializations.jl
+++ src/SimpleGraphs/specializations.jl
@@ -2,7 +2,7 @@
 """
     squash(g::Union{SimpleGraph, SimpleDiGraph}; alwayscopy=true)
 
-Specialised version of `Graphs::Squash` for `SimpleGraph` and `SimpleDiGraph`.
+Specialised version of `Graphs.squash` for `SimpleGraph` and `SimpleDiGraph`.
 If `alwayscopy` is `true`, the resulting graph will always be a copy, otherwise
 it can also be the original graph.
 """
