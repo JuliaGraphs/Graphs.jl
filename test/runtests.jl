349df89b4428888f0b7349e0f81d69cf54498779
aurorarossi <aurora98rossi@gmail.com>
2022-10-25 15:06:03 +0200

    Add verbose and remove long output

--- test/runtests.jl
+++ test/runtests.jl
@@ -92,9 +92,9 @@ tests = [
     "experimental/experimental"
 ]
 
-@testset "Graphs" begin
+@testset verbose=true "Graphs" begin
     for t in tests
         tp = joinpath(testdir, "$(t).jl")
         include(tp)
     end
-end
+end;
