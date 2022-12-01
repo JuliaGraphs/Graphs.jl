03357dc55bfe0ca131f788fbcb5f85dec51cde70
aurorarossi <aurora98rossi@gmail.com>
2022-12-01 14:51:47 +0100

    Fix format and change dimension graphs

--- test/operators.jl
+++ test/operators.jl
@@ -245,19 +245,19 @@
         end
     end
 
-    gx = SimpleGraph(100, 200)
-    gy = SimpleGraph(123, 234)
-    @testset "Graph product edge counts" for (g1,g2) in zip(testgraphs(gx),testgraphs(gy))
+    gx = SimpleGraph(10, 20)
+    gy = SimpleGraph(15, 34)
+    @testset "Graph product edge counts" for (g1, g2) in zip(testgraphs(gx), testgraphs(gy))
         v1 = nv(g1)
         v2 = nv(g2)
         e1 = ne(g1)
         e2 = ne(g2)
         # Edge counts from https://en.wikipedia.org/wiki/Graph_product
-        @test ne(cartesian_product(g1, g2)) == v1*e2 + v2*e1
-        @test ne(tensor_product(g1, g2)) == 2*e1*e2
-        @test ne(lexicographic_product(g1, g2)) == v1*e2 + e1*v2^2
-        @test ne(strong_product(g1, g2)) == v1*e2 + v2*e1 + 2*e1*e2
-        @test ne(disjunctive_product(g1, g2)) == v1^2*e2 + e1*v2^2 - 2*e1*e2
+        @test ne(cartesian_product(g1, g2)) == v1 * e2 + v2 * e1
+        @test ne(tensor_product(g1, g2)) == 2 * e1 * e2
+        @test ne(lexicographic_product(g1, g2)) == v1 * e2 + e1 * v2^2
+        @test ne(strong_product(g1, g2)) == v1 * e2 + v2 * e1 + 2 * e1 * e2
+        @test ne(disjunctive_product(g1, g2)) == v1^2 * e2 + e1 * v2^2 - 2 * e1 * e2
     end
 
     ## test subgraphs ##
