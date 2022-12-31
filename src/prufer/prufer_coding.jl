8cf308a6c53c5de2669ad771a0dad63510fe70a4
Simon <costesimon@live.fr>
2022-12-31 11:29:08 +0100

    treesize must be >2

--- src/prufer/prufer_coding.jl
+++ src/prufer/prufer_coding.jl
@@ -7,49 +7,53 @@ This is only for undirected graphs.
 """
 
 function is_tree(g::SimpleGraph)
-    return is_connected(g) && ne(g)==nv(g)-1
+    return is_connected(g) && ne(g) == nv(g) - 1
 end
 
 function _is_prufer(c)
-    ndims(c)==1 && maximum(c) <= length(c)+2
+    ℓ = length(c)
+    return ndims(c) == 1 && maximum(c) <= ℓ + 2 && ℓ >= 1
 end
 
-function _degree_from_prufer(c::Vector{Int64})
+function _degree_from_prufer(c::Vector{T}) where {T<:Integer}
     """
     Degree sequence from prufer code.
     Returns d such that d[i] = 1 + number of occurences of i in c
     """
-    n = length(c)+2
-    [count(==(i), c) for i in 1:n] .+ 1
+    n = length(c) + 2
+    return [count(==(i), c) for i in 1:n] .+ 1
 end
 
 function test_degree_sequence()
-    a = [1,2,3,4,4,4] # 8 nodes
-    _prufer_degree_sequence(a) == [2, 2, 2, 4, 1, 1, 1, 1]
+    a = [1, 2, 3, 4, 4, 4] # 8 nodes
+    return _prufer_degree_sequence(a) == [2, 2, 2, 4, 1, 1, 1, 1]
 end
 
 """
     prufer_decode(code)
 
 Returns the unique tree associated with the given (Prüfer) code. 
-Each tree of size n is associated with a Prüfer sequence (a[1], ..., a[n-2]) with 1 ⩽ a[i] ⩽ n. The sequence is constructed recursively by the leaf removal algoritm. At step k, the leaf with largest index is removed and its unique neighbor is added to the Prüfer sequence, giving a[k]. The decoding algorithm goes backward.  
+Each tree of size n is associated with a Prüfer sequence (a[1], ..., a[n-2]) with 1 ⩽ a[i] ⩽ n. The sequence is constructed recursively by the leaf removal algoritm. At step k, the leaf with smallest index is removed and its unique neighbor is added to the Prüfer sequence, giving a[k]. The decoding algorithm goes backward.  
 Ref: [Prüfer sequence on Wikipedia](https://en.wikipedia.org/wiki/Pr%C3%BCfer_sequence)
 """
 
-function prufer_decode(code::Array{T,1})::Graph where {T<:Integer}
-
-    !_is_prufer(code) && throw(ArgumentError("The code must be an Array{T,1} and must be a Prufer sequence. "))
+function prufer_decode(code::Array{T,1})::Graph{T} where {T<:Integer}
     n = length(code) + 2
+    !_is_prufer(code) && throw(
+        ArgumentError(
+            "The code must be an Array{T,1} and must be a Prufer sequence with length ⩾1. ",
+        ),
+    )
     d = _degree_from_prufer(code)
-    L = BinaryMaxHeap(findall(==(1),d))
+    L = BinaryMinHeap(findall(==(1), d))
     g = Graph(n, 0)
 
-    for i in 1:n-2
+    for i in 1:(n - 2)
         l = pop!(L) # extract leaf with priority rule (max)
         d[l] -= 1 # update degree sequence
-        add_edge!(g, l,code[i]) # add edge
+        add_edge!(g, l, code[i]) # add edge
         d[code[i]] -= 1 # update degree sequence
-        d[code[i]]==1 && push!(L, code[i]) # add new leaf if any
+        d[code[i]] == 1 && push!(L, code[i]) # add new leaf if any
     end
 
     add_edge!(g, pop!(L), pop!(L)) # add last leaf
@@ -60,31 +64,32 @@ end
 """
     prufer_encode(g)
 
-Given a tree (a connected minimal undirected graph), returns the unique Prüfer sequence associated with this tree. 
+Given a tree (a connected minimal undirected graph) of size n⩾2, returns the unique Prüfer sequence associated with this tree. 
 
-Each tree of size n is associated with a Prüfer sequence (a[1], ..., a[n-2]) with 1 ⩽ a[i] ⩽ n. The sequence is constructed recursively by the leaf removal algoritm. At step k, the leaf with largest index is removed and its unique neighbor is added to the Prüfer sequence, giving a[k]. 
+Each tree of size n ⩾ 2 is associated with a Prüfer sequence (a[1], ..., a[n-2]) with 1 ⩽ a[i] ⩽ n. The sequence is constructed recursively by the leaf removal algoritm. At step k, the leaf with smallest index is removed and its unique neighbor is added to the Prüfer sequence, giving a[k]. 
 
 Ref: [Prüfer sequence on Wikipedia](https://en.wikipedia.org/wiki/Pr%C3%BCfer_sequence)
 
 """
 
-function prufer_encode(G::Graph)::Array{Integer,1}
-
-    !is_tree(g) && throw(ArgumentError("The graph must be a tree. "))
+function prufer_encode(G::Graph{T})::Array{T,1} where {T<:Integer}
+    n = nv(G)
+    (!is_tree(G) || n <= 2) &&
+        throw(ArgumentError("The graph must be a tree with n ⩾ 3 vertices. "))
     g = copy(G)
-    n = nv(g)
-    code = zeros(Int, n-2)
+    code = zeros(Int, n - 2)
     d = degree(g)
-    L = BinaryMaxHeap(findall(==(1),d))
+    L = BinaryMinHeap(findall(==(1), d))
 
-    for i in 1:n-2
+    for i in 1:(n - 2)
         l = pop!(L)
-        v = neighbors(g,l)[1]
-        rem_edge!(g,l,v)
+        v = neighbors(g, l)[1]
+        rem_edge!(g, l, v)
         d[l] -= 1
         d[v] -= 1
-        d[v] == 1 && push!(L,v)
+        d[v] == 1 && push!(L, v)
         code[i] = v
     end
 
     return code
+end
--- test/prufer/prufer.jl
+++ test/prufer/prufer.jl
@@ -1,3 +1,15 @@
-@testset "Prufer" begin
-    @test true == true
+@testset "Prufer trees" begin
+    g1 = Graph(6)
+    for e in [(1, 4), (2, 4), (3, 4), (4, 5), (5, 6)]
+        add_edge!(g1)
+    end
+
+    g2 = path_graph(10)
+
+    for g in testgraphs(g1)
+        @test is_tree(g)
+        code = prufer_encode(g)
+        @test code = [4, 4, 4, 5]
+        @test prufer_decode(code) == g
+    end
 end
