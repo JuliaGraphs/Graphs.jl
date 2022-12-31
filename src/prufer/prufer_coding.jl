8cb40694536c13ad9020888940ea7b52a15267be
Simon <costesimon@live.fr>
2022-12-31 15:21:43 +0100

    fixed types

--- src/prufer/prufer_coding.jl
+++ src/prufer/prufer_coding.jl
@@ -1,32 +1,28 @@
 """
     is_tree(g)
 
-Returns true if g is a simple tree: that is, a simple connected graph, with nv-1 edges (nv = number of vertices). Trees are the minimal connected graphs; equivalently they have no cycles.  
+Returns true if g is a tree: that is, a simple, connected undirected graph, with nv-1 edges (nv = number of vertices). Trees are the minimal connected graphs; equivalently they have no cycles.  
 
-This is only for undirected graphs. 
+This function does not apply to directed graphs. Directed trees are sometimes called [polytrees](https://en.wikipedia.org/wiki/Polytree)). 
+
 """
 
-function is_tree(g::SimpleGraph)
+function is_tree(g::AbstractGraph)
+    is_directed(g) && throw(ArgumentError("g must be undirected. "))
     return is_connected(g) && ne(g) == nv(g) - 1
 end
 
 function _is_prufer(c)
-    ℓ = length(c)
-    return ndims(c) == 1 && maximum(c) <= ℓ + 2 && ℓ >= 1
+    return ndims(c) == 1 && !isempty(c) && maximum(c) <= length(c) + 2
 end
 
-function _degree_from_prufer(c::Vector{T}) where {T<:Integer}
+function _degree_from_prufer(c::Vector{T})::Vector{T} where {T<:Integer}
     """
     Degree sequence from prufer code.
     Returns d such that d[i] = 1 + number of occurences of i in c
     """
     n = length(c) + 2
-    return [count(==(i), c) for i in 1:n] .+ 1
-end
-
-function test_degree_sequence()
-    a = [1, 2, 3, 4, 4, 4] # 8 nodes
-    return _prufer_degree_sequence(a) == [2, 2, 2, 4, 1, 1, 1, 1]
+    return [T(count(==(i), c) + 1) for i in 1:n]
 end
 
 """
@@ -38,15 +34,15 @@ Ref: [Prüfer sequence on Wikipedia](https://en.wikipedia.org/wiki/Pr%C3%BCfer_s
 """
 
 function prufer_decode(code::Array{T,1})::Graph{T} where {T<:Integer}
-    n = length(code) + 2
     !_is_prufer(code) && throw(
         ArgumentError(
             "The code must be an Array{T,1} and must be a Prufer sequence with length ⩾1. ",
         ),
     )
+    n = length(code) + 2
     d = _degree_from_prufer(code)
-    L = BinaryMinHeap(findall(==(1), d))
-    g = Graph(n, 0)
+    L = BinaryMinHeap{T}(findall(==(1), d))
+    g = Graph{T}(n, 0)
 
     for i in 1:(n - 2)
         l = pop!(L) # extract leaf with priority rule (max)
@@ -62,11 +58,11 @@ function prufer_decode(code::Array{T,1})::Graph{T} where {T<:Integer}
 end
 
 """
-    prufer_encode(g)
+    prufer_encode(g::SimpleGraph)
 
-Given a tree (a connected minimal undirected graph) of size n⩾2, returns the unique Prüfer sequence associated with this tree. 
+Given a tree (a connected minimal undirected graph) of size n⩾3, returns the unique Prüfer sequence associated with this tree. 
 
-Each tree of size n ⩾ 2 is associated with a Prüfer sequence (a[1], ..., a[n-2]) with 1 ⩽ a[i] ⩽ n. The sequence is constructed recursively by the leaf removal algoritm. At step k, the leaf with smallest index is removed and its unique neighbor is added to the Prüfer sequence, giving a[k]. 
+Each tree of size n ⩾ 3 is associated with a Prüfer sequence (a[1], ..., a[n-2]) with 1 ⩽ a[i] ⩽ n. The sequence is constructed recursively by the leaf removal algoritm. At step k, the leaf with smallest index is removed and its unique neighbor is added to the Prüfer sequence, giving a[k]. 
 
 Ref: [Prüfer sequence on Wikipedia](https://en.wikipedia.org/wiki/Pr%C3%BCfer_sequence)
 
@@ -77,7 +73,7 @@ function prufer_encode(G::Graph{T})::Array{T,1} where {T<:Integer}
     (!is_tree(G) || n <= 2) &&
         throw(ArgumentError("The graph must be a tree with n ⩾ 3 vertices. "))
     g = copy(G)
-    code = zeros(Int, n - 2)
+    code = zeros(T, n - 2)
     d = degree(g)
     L = BinaryMinHeap(findall(==(1), d))
 
