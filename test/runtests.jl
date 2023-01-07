37f64fae7c91a98f7c026d35666fd0fbf097e773
Joseph Bradley <76974735+josephcbradley@users.noreply.github.com>
2023-01-07 12:12:16 +0000

    Typo in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -137,7 +137,7 @@ tests = [
     "spanningtrees/boruvka",
     "spanningtrees/kruskal",
     "spanningtrees/prim",
-    "spanningtrees/planar_maximally_filtered_graph.jl",
+    "spanningtrees/planar_maximally_filtered_graph",
     "steinertree/steiner_tree",
     "biconnectivity/articulation",
     "biconnectivity/biconnect",
