5795a1fbcaf0376d10bd59f7fbd8c019d3f34ebe
aurorarossi <aurora98rossi@gmail.com>
2023-03-10 14:39:28 +0100

    Remove g

--- src/persistence/common.jl
+++ src/persistence/common.jl
@@ -107,7 +107,7 @@ function savegraph(fn::AbstractString, g::AbstractSimpleGraph; compress=nothing)
 end
 
 """
-    savegraph(file, g, d, format=LGFormat)
+    savegraph(file, d, format=LGFormat)
 
 Save a dictionary of `graphname => graph` to `file` in the format `format`.
 Return the number of graphs written.
