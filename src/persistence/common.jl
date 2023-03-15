1a7044b5d4824c320c5d02a5b708293a606fdb4f
aurorarossi <aurora98rossi@gmail.com>
2023-03-15 22:05:36 +0100

    Add example

--- src/persistence/common.jl
+++ src/persistence/common.jl
@@ -112,6 +112,20 @@ end
 Save a dictionary `d` of `graphname => graph` to `file` in the format `format`.
 Return the number of graphs written.
 
+# Examples
+```jldoctest
+julia> g1 = SimpleGraph(5,8)
+{5, 8} undirected simple Int64 graph
+
+julia> g2 = SimpleGraph(7,10)
+{7, 10} undirected simple Int64 graph
+
+julia> d = Dict("graph_1" => g1, "graph_2" => g2);
+
+julia> savegraph("myfile.txt", d, LGFormat())
+2
+```
+
 ### Implementation Notes
 Will only work if the file format supports multiple graph types.
 """
