5fe300724d1511face808b733297afdb1a4c5aac
etienneINSA <etienne.mace_de_gastines@insa-rouen.fr>
2023-06-08 18:04:03 +0200

    astar with edge_weight

--- src/shortestpaths/astar.jl
+++ src/shortestpaths/astar.jl
@@ -5,12 +5,13 @@
 
 function reconstruct_path!(total_path, # a vector to be filled with the shortest path
     came_from, # a vector holding the parent of each node in the A* exploration
+    came_from_edge, # a vector holding the parent of each node in the A* exploration
     end_idx, # the end vertex
     g,  # the graph
-    ) where {E<:AbstractEdge}
+    )
     curr_idx = end_idx
     while came_from[curr_idx] != curr_idx
-        pushfirst!(total_path, E(came_from[curr_idx], curr_idx))
+        pushfirst!(total_path, came_from_edge[curr_idx])
         curr_idx = came_from[curr_idx]
     end
 end
@@ -20,8 +21,9 @@ function a_star_impl!(g::AbstractGraph{V, E}, # the graph
     open_set, # an initialized heap containing the active vertices
     closed_set, # an (initialized) color-map to indicate status of vertices
     g_score, # a vector holding g scores for each node
+    edge_weight,
     came_from, # a vector holding the parent of each node in the A* exploration
-    vertex_dist,
+    came_from_edge,
     heuristic,) where {V, E}
 
     total_path = Vector{E}()
@@ -30,27 +32,31 @@ function a_star_impl!(g::AbstractGraph{V, E}, # the graph
         current = dequeue!(open_set)
 
         if current == goal
-            reconstruct_path!(total_path, came_from, current, g, E)
+            reconstruct_path!(total_path, came_from, came_from_edge, current, g)
             return total_path
         end
 
         closed_set[current] = true
 
-        for neighbor in outneighbors(g, current)
+        for e in outedges(g, current)
+            neighbor = src(e) == current : dst(e) : src(e)
             closed_set[neighbor] && continue
 
-            tentative_g_score = g_score[current] + vertex_dist(g, current, neighbor)
+            tentative_g_score = g_score[current] + edge_weight(e)
 
             if tentative_g_score < g_score[neighbor]
                 g_score[neighbor] = tentative_g_score
                 priority = tentative_g_score + heuristic(neighbor)
                 open_set[neighbor] = priority
                 came_from[neighbor] = current
+                came_from_edge[neighbor] = e
             end
         end
+
     end
     return total_path
 end
+
 
 """
     a_star(g, s, t[, distmx][, heuristic][])
@@ -61,17 +67,15 @@ Compute a shortest path using the [A* search algorithm](http://en.wikipedia.org/
 - `g::AbstractGraph`: the graph
 - `s::Integer`: the source vertex
 - `t::Integer`: the target vertex
-- `vertex_dist::Function`: an optional function returning the distance between the vertices `u` and `v` in `g`
+- `edge_weight::Function`: an optional function returning the distance between the vertices `u` and `v` in `g`
 - `heuristic::Function`: an optional function mapping each vertex to a lower estimate of the remaining distance from `v` to `t`. It is set to `v -> 0` by default (which corresponds to Dijkstra's algorithm)
 """
 function a_star(g::AbstractGraph{V, E},  # the graph
-    s::AbstractVertex,                   # the start vertex
-    t::AbstractVertex,                   # the end vertex
-    vertex_dist::Function = (g, u, v) -> minimum(weight.(get_edges(g, u, v))),
-    distmx::AbstractMatrix{T}=weights(g),
-    heuristic::Function=n -> zero(T)) where {T, V<:AbstractVertex, E<:AbstractEdge}
-    # if we do checkbounds here, we can use @inbounds in a_star_impl!
-    checkbounds(distmx, Base.OneTo(nv(g)), Base.OneTo(nv(g)))
+    s,                   # the start vertex
+    t,                   # the end vertex
+    edge_weight::Function = e -> weight(e),
+    T,
+    heuristic::Function=n -> zero(T)) where {T, V, E<:AbstractEdge}
 
     open_set = PriorityQueue{V, T}()
     enqueue!(open_set, s, 0)
@@ -86,14 +90,17 @@ function a_star(g::AbstractGraph{V, E},  # the graph
     came_from = get_vertex_container(g, T)
     came_from[vertices(g)] .= s
 
+    came_from_edge = get_vertex_container(g, E)
+
     a_star_impl!(
         g,
         t,
         open_set,
         closed_set,
         g_score,
+        edge_weight,
         came_from,
-        distmx,
+        came_from_edge,
         heuristic,
     )
 end
@@ -110,9 +117,13 @@ Compute a shortest path using the [A* search algorithm](http://en.wikipedia.org/
 - `distmx::AbstractMatrix`: an optional (possibly sparse) `n × n` matrix of edge weights. It is set to `weights(g)` by default (which itself falls back on [`Graphs.DefaultDistance`](@ref)).
 - `heuristic::Function`: an optional function mapping each vertex to a lower estimate of the remaining distance from `v` to `t`. It is set to `v -> 0` by default (which corresponds to Dijkstra's algorithm)
 """
-a_star(g::AbstractGraph{V, E},  # the graph
-    s::AbstractVertex,          # the start vertex
-    t::AbstractVertex,          # the end vertex
-    distmx::AbstractMatrix{T}=weights(g),
-    heuristic::Function=n -> zero(T)) where {T, V<:AbstractVertex, E<:AbstractEdge}
-    = a_star(g, s, t, (g, s, t)->distmx[u, v], heuristic)
+function a_star(g::AbstractSimpleGraph,  # the graph
+    s,          # the start vertex
+    t,          # the end vertex
+    distmx::AbstractMatrix{T},
+    heuristic::Function=n -> zero(T)) where {T}
+
+    # if we do checkbounds here, we can use @inbounds in a_star_impl!
+    checkbounds(distmx, Base.OneTo(nv(g)), Base.OneTo(nv(g)))
+    return @inbounds a_star(g, s, t, e -> distmx[src(e), dst(e)], heuristic)
+end
