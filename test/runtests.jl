61ff3dcf06eaca1b01e6fa84ab907abb3188145f
Thomas Christensen <tchr@mit.edu>
2023-06-28 09:06:33 +0000

    actually include new tests in "test/runtests.jl"

--- test/runtests.jl
+++ test/runtests.jl
@@ -84,6 +84,7 @@ tests = [
     "traversals/maxadjvisit",
     "traversals/randomwalks",
     "traversals/diffusion",
+    "traversals/eulerian",
     "community/cliques",
     "community/core-periphery",
     "community/label_propagation",
