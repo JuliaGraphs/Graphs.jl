93257591a34d152d8544a00d8056695b4fb8c5cc
Thomas Christensen <tchr@mit.edu>
2023-06-28 18:42:24 +0000

    Doc nit

--- src/traversals/eulerian.jl
+++ src/traversals/eulerian.jl
@@ -5,7 +5,7 @@
 """
     eulerian(g::AbstractSimpleGraph{T}[, u::T]) --> T[]
 
-Compute an [Eulerian trail or cycle](https://en.wikipedia.org/wiki/Eulerian_path) through an
+Returns a [Eulerian trail or cycle](https://en.wikipedia.org/wiki/Eulerian_path) through an
 undirected graph `g`, starting at `u`, returning a vector listing the vertices of `g` in the
 order that they are traversed. If no such trail or cycle exists, throws an error.
 
