50b48c7cf9a68478ec16e28fcf99c29458416bc3
Simon Schölly <sischoel@gmail.com>
2023-07-05 10:19:18 +0200

    Use GenericGraph for testing centrality algorithms (#272)

--- test/centrality/katz.jl
+++ test/centrality/katz.jl
@@ -4,7 +4,7 @@
     add_edge!(g5, 2, 3)
     add_edge!(g5, 1, 3)
     add_edge!(g5, 3, 4)
-    for g in testdigraphs(g5)
+    for g in test_generic_graphs(g5)
         z = @inferred(katz_centrality(g, 0.4))
         @test round.(z, digits=2) == [0.32, 0.44, 0.62, 0.56]
     end
