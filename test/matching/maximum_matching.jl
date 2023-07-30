753dcd72cd3c559b3c718878ae7ad30099d23cdd
Robert Parker <robbybparker@gmail.com>
2023-07-30 09:46:15 -0600

    update tests to use maximum_matching

--- test/matching/maximum_matching.jl
+++ test/matching/maximum_matching.jl
@@ -17,6 +17,55 @@ function test_simple_example()
     add_edge!(g, (5, 6))
     add_edge!(g, (5, 9))
 
+    matching = maximum_cardinality_matching(g)
+    @test length(matching) == 10
+    for i in 1:10
+        @test i in keys(matching)
+        @test matching[i] in neighbors(g, i)
+    end
+end
+
+function test_simple_example_algorithm_argument()
+    # From the wikipedia page for the Hopcroft-Karp algorithm
+    # https://en.wikipedia.org/wiki/Hopcroft–Karp_algorithm
+    g = Graph()
+    add_vertices!(g, 10)
+    add_edge!(g, (1, 6))
+    add_edge!(g, (1, 7))
+    add_edge!(g, (2, 6))
+    add_edge!(g, (2, 10))
+    add_edge!(g, (3, 8))
+    add_edge!(g, (3, 9))
+    add_edge!(g, (4, 6))
+    add_edge!(g, (4, 10))
+    add_edge!(g, (5, 6))
+    add_edge!(g, (5, 9))
+
+    algorithm = HopcroftKarpAlgorithm()
+    matching = maximum_cardinality_matching(g, algorithm)
+    @test length(matching) == 10
+    for i in 1:10
+        @test i in keys(matching)
+        @test matching[i] in neighbors(g, i)
+    end
+end
+
+function test_simple_example_hopcroft_karp()
+    # From the wikipedia page for the Hopcroft-Karp algorithm
+    # https://en.wikipedia.org/wiki/Hopcroft–Karp_algorithm
+    g = Graph()
+    add_vertices!(g, 10)
+    add_edge!(g, (1, 6))
+    add_edge!(g, (1, 7))
+    add_edge!(g, (2, 6))
+    add_edge!(g, (2, 10))
+    add_edge!(g, (3, 8))
+    add_edge!(g, (3, 9))
+    add_edge!(g, (4, 6))
+    add_edge!(g, (4, 10))
+    add_edge!(g, (5, 6))
+    add_edge!(g, (5, 9))
+
     matching = hopcroft_karp_matching(g)
     @test length(matching) == 10
     for i in 1:10
@@ -51,7 +100,7 @@ function test_imperfect_matching()
     add_edge!(g, (7, 16))
     add_edge!(g, (8, 16))
 
-    matching = hopcroft_karp_matching(g)
+    matching = maximum_cardinality_matching(g)
     @test length(matching) == 14
 
     possibly_unmatched_1 = Set([5, 6, 7, 8])
@@ -74,7 +123,7 @@ end
 
 function test_complete_bipartite()
     g = complete_bipartite_graph(10, 20)
-    matching = hopcroft_karp_matching(g)
+    matching = maximum_cardinality_matching(g)
     @test length(matching) == 20
     for i in 1:10
         @test i in keys(matching)
@@ -84,11 +133,16 @@ end
 
 function test_not_bipartite()
     g = complete_graph(5)
-    @test_throws(ArgumentError, hopcroft_karp_matching(g))
+    @test_throws(ArgumentError, maximum_cardinality_matching(g))
 end
 
-@testset "Hopcroft-Karp matching" begin
+@testset "Maximum cardinality matching" begin
     test_simple_example()
+    test_simple_example_algorithm_argument()
+    test_simple_example_hopcroft_karp()
+
+    # NOTE: Right now there is only one algorithm to test. When we add more,
+    # we should loop over the algorithms to run these tests for each.
     test_imperfect_matching()
     test_complete_bipartite()
     test_not_bipartite()
