15e664009b26eb57306e88b6eb97ab92c1a09689
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-09-14 15:28:53 +0200

    Improve docstrings for all shortest path states (#302)

--- src/shortestpaths/yen.jl
+++ src/shortestpaths/yen.jl
@@ -1,11 +1,15 @@
 # TODO this algorithm does not work with abitrary AbstractGraph yet,
 # as it relies on rem_edge! and deepcopy
 
-
 """
     struct YenState{T, U}
 
 Designed for yen k-shortest-paths calculations.
+
+# Fields
+
+- `dists::Vector{T}`: `dists[k]` is the length of the `k`-th shortest path from the source to the target
+- `paths::Vector{Vector{U}}`: `paths[k]` is the description of the `k`-th shortest path (as a sequence of vertices) from the source to the target 
 """
 struct YenState{T,U<:Integer} <: AbstractPathState
     dists::Vector{T}
@@ -25,8 +29,8 @@ function yen_k_shortest_paths(
     target::U,
     distmx::AbstractMatrix{T}=weights(g),
     K::Int=1;
-    maxdist=typemax(T)) where {T<:Real} where {U<:Integer}
-
+    maxdist=typemax(T),
+) where {T<:Real} where {U<:Integer}
     source == target && return YenState{T,U}([U(0)], [[source]])
 
     dj = dijkstra_shortest_paths(g, source, distmx; maxdist)
@@ -39,7 +43,7 @@ function yen_k_shortest_paths(
     B = PriorityQueue()
     gcopy = deepcopy(g)
 
-    for k in 1:(K-1)
+    for k in 1:(K - 1)
         for j in 1:length(A[k])
             # Spur node is retrieved from the previous k-shortest path, k − 1
             spurnode = A[k][j]
@@ -52,7 +56,7 @@ function yen_k_shortest_paths(
             for ppath in A
                 if length(ppath) > j && rootpath == ppath[1:j]
                     u = ppath[j]
-                    v = ppath[j+1]
+                    v = ppath[j + 1]
                     if has_edge(gcopy, u, v)
                         rem_edge!(gcopy, u, v)
                         push!(edgesremoved, (u, v))
@@ -62,7 +66,7 @@ function yen_k_shortest_paths(
 
             # Remove node of root path and calculate dist of it
             distrootpath = zero(T)
-            for n in 1:(length(rootpath)-1)
+            for n in 1:(length(rootpath) - 1)
                 u = rootpath[n]
                 nei = copy(neighbors(gcopy, u))
                 for v in nei
@@ -71,7 +75,7 @@ function yen_k_shortest_paths(
                 end
 
                 # Evaluate distance of root path
-                v = rootpath[n+1]
+                v = rootpath[n + 1]
                 distrootpath += distmx[u, v]
             end
 
@@ -80,7 +84,7 @@ function yen_k_shortest_paths(
             spurpath = enumerate_paths(djspur)[target]
             if !isempty(spurpath)
                 # Entire path is made up of the root path and spur path
-                pathtotal = [rootpath[1:(end-1)]; spurpath]
+                pathtotal = [rootpath[1:(end - 1)]; spurpath]
                 distpath = distrootpath + djspur.dists[target]
                 # Add the potential k-shortest path to the heap
                 if !haskey(B, pathtotal)
