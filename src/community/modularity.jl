0fa5f52856f7f779e8f2ca49bc6cee26e5e19b50
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-09-28 10:09:17 +0200

    Fix most failing doctests (#305)

--- src/community/modularity.jl
+++ src/community/modularity.jl
@@ -53,23 +53,21 @@ julia> modularity(barbell, [1, 1, 1, 2, 2, 2])
 julia> modularity(barbell, [1, 1, 1, 2, 2, 2], γ=0.5)
 0.6071428571428571  
 ```
-```
-julia> using SimpleWeightedGraphs
+```jldoctest
+julia> using Graphs
 
-julia> triangle = SimpleWeightedGraph(3);
-
-julia> add_edge!(triangle, 1, 2, 1);
-
-julia> add_edge!(triangle, 2, 3, 1);
-
-julia> add_edge!(triangle, 3, 1, 1);
+julia> triangle = cycle_graph(3);
 
 julia> barbell = blockdiag(triangle, triangle);
 
-julia> add_edge!(barbell, 1, 4, 5); # this edge has a weight of 5
+julia> add_edge!(barbell, 1, 4);
 
-julia> modularity(barbell, [1, 1, 1, 2, 2, 2])
-0.045454545454545456
+julia> distmx = Matrix(weights(barbell));
+
+julia> distmx[1, 4] = distmx[4, 1] = 5;  # additional edge has a weight of 5
+
+julia> round(modularity(barbell, [1, 1, 1, 2, 2, 2]; distmx), digits=6)
+0.045455
 ```
 """
 function modularity(
