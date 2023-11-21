322cf701b56fe5aa720b9c9f22de5d59851b49b0
Oleg Fafurin <olegfafurin1@gmail.com>
2023-11-21 13:47:02 +0100

    fix algo

--- src/community/greedy_modularity.jl
+++ src/community/greedy_modularity.jl
@@ -1,33 +1,41 @@
-function greedy_modularity(g::AbstractGraph)
+function community_detection_greedy_modularity(g::AbstractGraph)
     n = length(vertices(g))
     c = Vector(1:n)
     cs = Vector()
-    qs = fill(-1., n)
+    qs = fill(-1.0, n)
     Q, e, a = compute_modularity(g, c)
+    println(Q)
     push!(cs, c)
     qs[1] = Q
-    for i=1:n-1
+    for i in 1:(n - 1)
         Q = modularity_greedy_step!(g, Q, e, a, c)
-        push!(cs, c)
-        qs[i+1] = Q
+        println(Q)
+        push!(cs, copy(c))
+        qs[i + 1] = Q
     end
     imax = argmax(qs)
     return rewrite_class_ids(cs[imax])
 end
 
-function modularity_greedy_step!(g::AbstractGraph, Q::Float64, e::Matrix{<:AbstractFloat}, a::AbstractVector{<:AbstractFloat},  c::AbstractVector{<:Integer})
+function modularity_greedy_step!(
+    g::AbstractGraph,
+    Q::Float64,
+    e::Matrix{<:AbstractFloat},
+    a::AbstractVector{<:AbstractFloat},
+    c::AbstractVector{<:Integer},
+)
     m = 2 * length(edges(g))
     n = length(vertices(g))
     dq_max = -1
-    tried = Set{Tuple{Int64, Int64}}()
-    to_merge::Tuple{Integer, Integer} = (0,0)
+    tried = Set{Tuple{Int64,Int64}}()
+    to_merge::Tuple{Integer,Integer} = (0, 0)
     tried = Set()
     for edge in edges(g)
         u = min(src(edge), dst(edge))
         v = max(src(edge), dst(edge))
-        if !((u, v) in tried)
-            push!(tried, (u,v))
-            dq = 2* (e[u,v] / m - a[u]*a[v] / m^2)
+        if c[u] != c[v] && !((c[u], c[v]) in tried)
+            push!(tried, (c[u], c[v]))
+            dq = (e[c[u], c[v]] / m - a[c[u]] * a[c[v]] / m^2)
             if dq > dq_max
                 dq_max = dq
                 to_merge = (c[u], c[v])
@@ -35,24 +43,24 @@ function modularity_greedy_step!(g::AbstractGraph, Q::Float64, e::Matrix{<:Abstr
         end
     end
     c1, c2 = to_merge
-    for i=1:n
+    println(dq_max, " ", c1, " ", c2)
+    for i in 1:n
         e[c1, i] += e[c2, i]
     end
-    for i=1:n
+    for i in 1:n
         if i == c2
             continue
         end
         e[i, c1] += e[i, c2]
     end
     a[c1] = a[c1] + a[c2]
-    for i=1:n
+    for i in 1:n
         if c[i] == c2
             c[i] = c1
         end
     end
-    return Q
+    return Q + 2 * dq_max
 end
-
 
 function compute_modularity(g::AbstractGraph, c::AbstractVector{<:Integer})
     Q = 0
@@ -64,13 +72,13 @@ function compute_modularity(g::AbstractGraph, c::AbstractVector{<:Integer})
         for v in neighbors(g, u)
             if c[u] == c[v]
                 Q += 1
-                e[c[i], c[j]] += 1
             end
+            e[c[u], c[v]] += 1
             a[c[u]] += 1
         end
     end
     Q *= m
-    for i=1:n_groups
+    for i in 1:n_groups
         Q -= a[i]^2
     end
     Q /= m^2
@@ -80,7 +88,7 @@ end
 function rewrite_class_ids(v::AbstractVector{<:Integer})
     d = Dict()
     vn = zeros(Int64, length(v))
-    for i=eachindex(v)
+    for i in eachindex(v)
         if !(v[i] in keys(d))
             d[v[i]] = length(d) + 1
         end
