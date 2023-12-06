58fcc8cd99a225de7f7596167df59c4d1967d3ca
Oleg Fafurin <olegfafurin1@gmail.com>
2023-12-06 13:44:48 +0100

    slow algo: bugfixes, performance optimization, tests with SBM

--- src/community/greedy_modularity.jl
+++ src/community/greedy_modularity.jl
@@ -1,18 +1,18 @@
-function community_detection_greedy_modularity(g::AbstractGraph)
+function community_detection_greedy_modularity(g::AbstractGraph; weights::AbstractMatrix=weights(g))
     if is_directed(g)
         throw(ArgumentError("The graph must not be directed"))
     end
     n = nv(g)
     c = Vector{Int}(1:n)
     cs = Vector{Vector{Int}}(undef, n)
-    w = weights(g)
-    T = float(eltype(w))
+    T = float(eltype(weights))
     qs = Vector{T}(undef, n)
-    Q, e, a = compute_modularity(g, c, w)
+    Q, e, a = compute_modularity(g, c, weights)
+    m = sum(a)
     cs[1] = copy(c)
     qs[1] = Q
     for i in 2:n
-        Q = modularity_greedy_step!(g, Q, e, a, c)
+        Q = modularity_greedy_step!(g, Q, e, a, c, m)
         cs[i] = copy(c)
         qs[i] = Q
     end
@@ -26,18 +26,14 @@ function modularity_greedy_step!(
     e::AbstractMatrix{T},
     a::AbstractVector{T},
     c::AbstractVector{<:Integer},
+    m::T
 ) where {T}
-    m = 2 * ne(g)
     n = nv(g)
     dq_max::typeof(Q) = typemin(Q)
-    tried = Set{Tuple{Integer,Integer}}()
-    to_merge::Tuple{Integer,Integer} = (0, 0)
-    tried = Set{Tuple{Integer,Integer}}()
+    to_merge::Tuple{Int,Int} = (0, 0)
     for edge in edges(g)
-        u = min(src(edge), dst(edge))
-        v = max(src(edge), dst(edge))
-        if c[u] != c[v] && !((c[u], c[v]) in tried)
-            push!(tried, (c[u], c[v]))
+        u, v = src(edge), dst(edge)
+        if c[u] != c[v]
             dq = (e[c[u], c[v]] / m - a[c[u]] * a[c[v]] / m^2)
             if dq > dq_max
                 dq_max = dq
@@ -45,29 +41,33 @@ function modularity_greedy_step!(
             end
         end
     end
-    c1, c2 = to_merge
-    for i in 1:n
-        e[c1, i] += e[c2, i]
-    end
-    for i in 1:n
-        if i == c2
-            continue
+    if dq_max > zero(typeof(Q))
+        c1, c2 = to_merge
+        for i in 1:n
+            e[c1, i] += e[c2, i]
         end
-        e[i, c1] += e[i, c2]
-    end
-    a[c1] = a[c1] + a[c2]
-    for i in 1:n
-        if c[i] == c2
-            c[i] = c1
+        for i in 1:n
+            if i == c2
+                continue
+            end
+            e[i, c1] += e[i, c2]
         end
+        a[c1] = a[c1] + a[c2]
+        for i in 1:n
+            if c[i] == c2
+                c[i] = c1
+            end
+        end
+        return Q + 2 * dq_max
+    else
+        return Q
     end
-    return Q + 2 * dq_max
 end
 
 function compute_modularity(g::AbstractGraph, c::AbstractVector{<:Integer}, w::AbstractArray)
     modularity_type = float(eltype(w))
     Q = zero(modularity_type)
-    m = sum([w[src(e), dst(e)] for e in edges(g)]) * 2
+    m = sum(w[src(e), dst(e)] for e in edges(g)) * 2
     n_groups = maximum(c)
     a = zeros(modularity_type, n_groups)
     e = zeros(modularity_type, n_groups, n_groups)
@@ -89,7 +89,7 @@ function compute_modularity(g::AbstractGraph, c::AbstractVector{<:Integer}, w::A
 end
 
 function rewrite_class_ids(v::AbstractVector{<:Integer})
-    d = Dict{Integer, Integer}()
+    d = Dict{Int, Int}()
     vn = zeros(Int64, length(v))
     for i in eachindex(v)
         if !(v[i] in keys(d))
