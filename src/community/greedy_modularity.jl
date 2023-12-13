5412d417aad1e5aa2cf0cc6e12c89c8618348dcb
Oleg Fafurin <olegfafurin1@gmail.com>
2023-12-13 15:59:09 +0100

    remove history of modularity opt steps + add sparse matrix

--- src/community/greedy_modularity.jl
+++ src/community/greedy_modularity.jl
@@ -1,23 +1,27 @@
-function community_detection_greedy_modularity(g::AbstractGraph; weights::AbstractMatrix=weights(g))
+using SparseArrays: spzeros, findnz, sparse, rowvals, nonzeros, nzrange, dropzeros!
+
+function community_detection_greedy_modularity(
+    g::AbstractGraph; weights::AbstractMatrix=weights(g)
+)
     if is_directed(g)
         throw(ArgumentError("The graph must not be directed"))
     end
     n = nv(g)
     c = Vector{Int}(1:n)
-    cs = Vector{Vector{Int}}(undef, n)
-    T = float(eltype(weights))
-    qs = Vector{T}(undef, n)
     Q, e, a = compute_modularity(g, c, weights)
     m = sum(a)
-    cs[1] = copy(c)
-    qs[1] = Q
+    Q_max = Q
+    c_best = copy(c)
     for i in 2:n
         Q = modularity_greedy_step!(g, Q, e, a, c, m)
-        cs[i] = copy(c)
-        qs[i] = Q
+        if Q_max < Q
+            Q_max = Q
+            c_best = copy(c)
+        else
+            break
+        end
     end
-    imax = argmax(qs)
-    return rewrite_class_ids(cs[imax])
+    return rewrite_class_ids(c_best)
 end
 
 function modularity_greedy_step!(
@@ -26,18 +30,24 @@ function modularity_greedy_step!(
     e::AbstractMatrix{T},
     a::AbstractVector{T},
     c::AbstractVector{<:Integer},
-    m::T
+    m::T,
 ) where {T}
     n = nv(g)
     dq_max::typeof(Q) = typemin(Q)
-    to_merge::Tuple{Int,Int} = (0, 0)
-    for edge in edges(g)
-        u, v = src(edge), dst(edge)
-        if c[u] != c[v]
-            dq = (e[c[u], c[v]] / m - a[c[u]] * a[c[v]] / m^2)
-            if dq > dq_max
-                dq_max = dq
-                to_merge = (c[u], c[v])
+    to_merge = (0, 0)
+    _, y = size(e)
+    rows = rowvals(e)
+    vals = nonzeros(e)
+    for col in 1:y
+        for i in nzrange(e, col)
+            row = rows[i]
+            value = vals[i]
+            if row != col
+                dq = (value / m - a[row] * a[col] / m^2)
+                if dq > dq_max
+                    dq_max = dq
+                    to_merge = (row, col)
+                end
             end
         end
     end
@@ -45,6 +55,7 @@ function modularity_greedy_step!(
         c1, c2 = to_merge
         for i in 1:n
             e[c1, i] += e[c2, i]
+            e[c2, i] = 0
         end
         for i in 1:n
             if i == c2
@@ -52,6 +63,8 @@ function modularity_greedy_step!(
             end
             e[i, c1] += e[i, c2]
         end
+        e[:, c2] .= 0
+        dropzeros!(e)
         a[c1] = a[c1] + a[c2]
         for i in 1:n
             if c[i] == c2
@@ -64,20 +77,26 @@ function modularity_greedy_step!(
     end
 end
 
-function compute_modularity(g::AbstractGraph, c::AbstractVector{<:Integer}, w::AbstractArray)
+function compute_modularity(
+    g::AbstractGraph, c::AbstractVector{<:Integer}, w::AbstractArray
+)
     modularity_type = float(eltype(w))
     Q = zero(modularity_type)
     m = sum(w[src(e), dst(e)] for e in edges(g); init=Q) * 2
     n_groups = maximum(c)
     a = zeros(modularity_type, n_groups)
-    e = zeros(modularity_type, n_groups, n_groups)
+    ei, ej = Vector{Int}(), Vector{Int}()
+    ev = Vector{modularity_type}()
+    e = spzeros(modularity_type, n_groups, n_groups)
     m == 0 && return 0.0, e, a
     for u in vertices(g)
         for v in neighbors(g, u)
             if c[u] == c[v]
                 Q += w[u, v]
             end
-            e[c[u], c[v]] += w[u, v]
+            push!(ei, c[u])
+            push!(ej, c[v])
+            push!(ev, w[u, v])
             a[c[u]] += w[u, v]
         end
     end
@@ -86,11 +105,11 @@ function compute_modularity(g::AbstractGraph, c::AbstractVector{<:Integer}, w::A
         Q -= a[i]^2
     end
     Q /= m^2
-    return Q, e, a
+    return Q, sparse(ei, ej, ev), a
 end
 
 function rewrite_class_ids(v::AbstractVector{<:Integer})
-    d = Dict{Int, Int}()
+    d = Dict{Int,Int}()
     vn = zeros(Int64, length(v))
     for i in eachindex(v)
         if !(v[i] in keys(d))
