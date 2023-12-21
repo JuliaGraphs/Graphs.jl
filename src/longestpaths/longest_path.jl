4b782a010c2810ab95df36dca63988e89a456076
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-12-21 09:44:07 +0100

    Simplify

--- src/longestpaths/longest_path.jl
+++ src/longestpaths/longest_path.jl
@@ -1,43 +1,17 @@
 """
     dag_longest_path(g; distmx=weights(g), topological_order=topological_sort_by_dfs(g))
 
-DAG longest path algorithm.
-Return a longest path of `g`.
-The parameter `distmx` defines the distance matrix, while
-the parameter `topological_order` defines a topological order of the `vertices(g)`.
+Find a longest path in a directed acyclic graph `g`.
+
+The parameter `distmx` defines the distance matrix, while the parameter `topological_order` defines a topological order of the `vertices(g)`.
 
 # Examples
 ```jldoctest
 julia> using Graphs
 
-julia> g = random_orientation_dag(complete_graph(10))
+julia> g = random_orientation_dag(complete_graph(10));
+
 julia> dag_longest_path(g)
-10-element Vector{Int64}:
-  9
-  1
-  5
-  2
-  4
-  6
-  3
- 10
-  8
-  7
-
-julia> dag_longest_path(g, topological_order = topological_sort_by_dfs(g))
-10-element Vector{Int64}:
-  9
-  1
-  5
-  2
-  4
-  6
-  3
- 10
-  8
-  7
-
-julia> dag_longest_path(g, distmx = weights(g), topological_order = topological_sort_by_dfs(g))
 10-element Vector{Int64}:
   9
   1
@@ -53,38 +27,34 @@ julia> dag_longest_path(g, distmx = weights(g), topological_order = topological_
 """
 function dag_longest_path end
 
-function dag_longest_path(
-        g::SimpleDiGraph{U}; 
-        distmx::AbstractMatrix{T} = weights(g), 
-        topological_order::Vector{U} = topological_sort_by_dfs(g)
-    ) where {T<:Real} where {U<:Integer}
-    n::Int = nv(g)
-    path::Vector{U} = Vector{U}()
+@traitfn function dag_longest_path(
+    g::AG::IsDirected;
+    distmx::AbstractMatrix{T}=weights(g),
+    topological_order::Vector{U}=topological_sort_by_dfs(g),
+) where {T<:Real,U<:Integer,AG<:AbstractGraph{U}}
+    n = nv(g)
+    path = U[]
 
     # edge case
     n == 0 && return path
 
-    dist::Vector{U} = Vector{U}(zeros(n))
-    pred::Vector{U} = Vector{U}(1:n)
+    dist = zeros(T, n)
+    pred = collect(vertices(g))
 
     # get distances
     for v in topological_order
         for u in inneighbors(g, v)
-
-            newdist::U = dist[u] + distmx[u, v]
-
+            newdist = dist[u] + distmx[u, v]
             if newdist > dist[v]
                 dist[v] = newdist
                 pred[v] = u
             end
-
         end
     end
 
     # retrieve path 
-    v = argmax(x -> dist[x], vertices(g))
+    v = argmax(dist)
     push!(path, v)
-
     while pred[v] != v
         v = pred[v]
         push!(path, v)
--- test/longestpaths/longest_path.jl
+++ test/longestpaths/longest_path.jl
@@ -1,24 +1,24 @@
 @testset "Longest path" begin
     # empty DAG
-    g::DiGraph = DiGraph()
-    @test dag_longest_path(g) == Vector{Int}()
+    g = DiGraph()
+    @test dag_longest_path(g) == Int[]
 
     # unweighted DAG
-    g = DiGraph()
-    V::Vector{Int} = Vector{Int}(1:7)
-    A = Vector{Tuple{Int, Int}}([(1, 2), (2, 3), (2, 4), (3, 5), (5, 6), (3, 7)])
-    [add_vertex!(g) for _ in V]
-    [add_edge!(g, edge) for edge in A]
-    @test dag_longest_path(g) == Vector{Int}([1, 2, 3, 5, 6])
+    g = DiGraph(7)
+    A = [(1, 2), (2, 3), (2, 4), (3, 5), (5, 6), (3, 7)]
+    for (i, j) in A
+        add_edge!(g, i, j)
+    end
+    @test dag_longest_path(g) == [1, 2, 3, 5, 6]
 
     # weighted DAG
-    g = DiGraph()
-    V = Vector{Int}(1:6)
-    A = Vector{Tuple{Int, Int, Float64}}([(1, 2, -5), (2, 3, 1), (3, 4, 1), (4, 5, 0), (3, 5, 4), (1, 6, 2)])
-    n::Int = length(V)
-    distmx::Matrix{Float64} = Matrix{Float64}(undef, n, n)
-    [(distmx[i, j] = dist) for (i, j, dist) in A]
-    [add_vertex!(g) for _ in V]
-    [add_edge!(g, (i, j)) for (i, j, _) in A]
-    @test dag_longest_path(g; distmx = distmx) == Vector{Int}([2, 3, 5])
+    n = 6
+    g = DiGraph(n)
+    A = [(1, 2, -5), (2, 3, 1), (3, 4, 1), (4, 5, 0), (3, 5, 4), (1, 6, 2)]
+    distmx = fill(NaN, n, n)
+    for (i, j, dist) in A
+        add_edge!(g, (i, j))
+        distmx[i, j] = dist
+    end
+    @test dag_longest_path(g; distmx=distmx) == [2, 3, 5]
 end
