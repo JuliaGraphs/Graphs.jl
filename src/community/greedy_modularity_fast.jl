051520265ecaccc7ca916823ddfb807135a94975
Oleg Fafurin <olegfafurin1@gmail.com>
2024-01-22 04:39:40 +0100

    add fast algorithm working (mind the precision)

--- src/community/greedy_modularity_fast.jl
+++ src/community/greedy_modularity_fast.jl
@@ -4,26 +4,23 @@ function community_detection_greedy_modularity_fast(g::AbstractGraph; weights::A
     end
     n = nv(g)
     c = Vector{Int}(1:n)
-    dq_dict, dq_heap, dq_global_heap, a = compute_dq(g, c, weights)
+    dq_dict, dq_heap, dq_global_heap, a, m = compute_dq(g, c, weights)
     modularity_type = float(eltype(weights))
-    for _ in 2:n
-        try
-            (u,v), dq = dequeue_pair!(dq_global_heap)
-        catch err
-            if isa(err, BoundsError)
-                break
-            end
-            throw(error("unknown error at call to priority queue"))
+    empty_row_heap = PriorityQueue{Tuple{Int, Int}, Tuple{modularity_type, Tuple{Int, Int}}}(Base.Order.Reverse) # placeholder, lasts empty forever
+    while length(dq_global_heap) > 1
+        (u,v), (dq, _) = dequeue_pair!(dq_global_heap)
+        if dq <= zero(modularity_type)
+            return rewrite_class_ids(c)
         end
         dequeue!(dq_heap[u])
         if !isempty(dq_heap[u])
-            enqueue!(dq_global_heap, first(dq_heap[u]))
+            enqueue!(dq_global_heap, peek(dq_heap[u]))
         end
-        if first(dq_heap[v])[1] == (v,u)
+        if peek(dq_heap[v])[1] == (v,u)
             dequeue!(dq_heap[v])
             delete!(dq_global_heap, (v,u))
             if !isempty(dq_heap[v])
-                enqueue!(dq_global_heap, first(dq_heap[v]))
+                enqueue!(dq_global_heap, peek(dq_heap[v]))
             end
         else
             delete!(dq_heap[v], (v,u))
@@ -31,8 +28,8 @@ function community_detection_greedy_modularity_fast(g::AbstractGraph; weights::A
 
         c[c .== u] .= v
 
-        neighbors_u = keys(dq_dict[u])
-        neighbors_v = keys(dq_dict[v])
+        neighbors_u = setdiff(keys(dq_dict[u]), v)
+        neighbors_v = setdiff(keys(dq_dict[v]), u)
         neighbors_all = union(neighbors_u, neighbors_v)
         neighbors_common = intersect(neighbors_u, neighbors_v)
         
@@ -40,23 +37,23 @@ function community_detection_greedy_modularity_fast(g::AbstractGraph; weights::A
             if w in neighbors_common
                 dq_w = dq_dict[v][w] + dq_dict[u][w]
             elseif w in neighbors_v
-                dq_w = dq_dict[v][w] - 2 * a[u] * a[w]
+                dq_w = dq_dict[v][w] - a[u] * a[w] / m^2
             else
-                dq_w = dq_dict[v][w] - 2 * a[v] * a[w]
+                dq_w = dq_dict[u][w] - a[v] * a[w] / m^2
             end
             for (row, column) in ((v, w), (w, v))
                 dq_heap_row = dq_heap[row]
                 dq_dict[row][column] = dq_w
                 if !isempty(dq_heap_row)
-                    oldmax = first(dq_heap_row)
+                    oldmax = peek(dq_heap_row)
                 else
                     oldmax = nothing
                 end
-                dq_heap_row[(row,column)] = dq_w # update or insert
+                dq_heap_row[(row,column)] = (dq_w, (-row, -column)) # update or insert
                 if isnothing(oldmax)
-                    dq_global_heap[(row, column)] = dq_w
+                    dq_global_heap[(row, column)] = (dq_w, (-row, -column))
                 else
-                    newmax = first(dq_heap_row)
+                    newmax = peek(dq_heap_row)
                     if newmax != oldmax
                         delete!(dq_global_heap, oldmax[1]) ## is it still there?
                         enqueue!(dq_global_heap, newmax)
@@ -70,11 +67,11 @@ function community_detection_greedy_modularity_fast(g::AbstractGraph; weights::A
             if w != v
                 for (row, column) in ((w,u), (u,w))
                     dq_heap_row = dq_heap[row]
-                    if first(dq_heap_row)[1] == (row, column)
+                    if peek(dq_heap_row)[1] == (row, column)
                         dequeue!(dq_heap_row)
                         delete!(dq_global_heap, (row, column))
                         if !isempty(dq_heap_row)
-                            enqueue!(dq_global_heap, first(dq_heap_row))
+                            enqueue!(dq_global_heap, peek(dq_heap_row))
                         end
                     else
                         delete!(dq_heap_row, (row, column))
@@ -83,7 +80,7 @@ function community_detection_greedy_modularity_fast(g::AbstractGraph; weights::A
             end
         end
         delete!(dq_dict, u)
-        dq_heap[u] = PriorityQueue{Tuple{Int, Int}, modularity_type}(Base.Order.Reverse) # placeholder, lasts empty forever
+        dq_heap[u] = empty_row_heap
         a[v] += a[u]
         a[u] = 0
     end
@@ -98,8 +95,7 @@ function compute_dq(
     m = sum(w[src(e), dst(e)] for e in edges(g); init=Q_zero) * 2
     n_groups = maximum(c)
     a = zeros(modularity_type, n_groups)
-    # m == 0 && return 0.0, spzeros(modularity_type, n_groups, n_groups), a
-    dq_dict = DefaultDict{Int, DefaultDict}(() -> DefaultDict{Int,modularity_type}(Q_zero))
+    dq_dict = Dict(v => DefaultDict{Int, modularity_type}(Q_zero) for v in vertices(g))
 
     for u in vertices(g)
         for v in neighbors(g, u)
@@ -114,15 +110,8 @@ function compute_dq(
         end
     end
 
-    dq_heap = Dict(u=>PriorityQueue{Tuple{Int, Int}, modularity_type}(Base.Order.Reverse, (u,v)=> dq for (v, dq) in dq_dict[u]) for u in vertices(g))
+    dq_heap = Dict(u=>PriorityQueue{Tuple{Int, Int}, Tuple{modularity_type, Tuple{Int, Int}}}(Base.Order.Reverse, (u,v)=> (dq, (-u,-v)) for (v, dq) in dq_dict[u]) for u in vertices(g))
     v_connected = filter(v -> !isempty(dq_heap[v]), vertices(g))
-    global_heap = PriorityQueue{Tuple{Int, Int}, modularity_type}(Base.Order.Reverse, first(dq_heap[v]) for v in v_connected)
-    return dq_dict, dq_heap, global_heap, a
+    global_heap = PriorityQueue{Tuple{Int, Int}, Tuple{modularity_type, Tuple{Int, Int}}}(Base.Order.Reverse, peek(dq_heap[v]) for v in v_connected)
+    return dq_dict, dq_heap, global_heap, a, m
 end
-
-# g = SimpleGraph(4)
-# add_edge!(g,1,2)
-# add_edge!(g,3,4)
-# w = weights(g)
-# c = 1:4
-# compute_dq(g,c,w)
