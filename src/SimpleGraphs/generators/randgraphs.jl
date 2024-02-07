11fe08f87b15bd8aa78ecf787c822bcb5e6a12c6
Henrik Wolf <henrik-wolf@freenet.de>
2024-02-07 22:54:45 +0100

    added note

--- src/SimpleGraphs/generators/randgraphs.jl
+++ src/SimpleGraphs/generators/randgraphs.jl
@@ -131,7 +131,12 @@ end
 
 Create an [Erdős–Rényi](http://en.wikipedia.org/wiki/Erdős–Rényi_model)
 random graph with `n` vertices. Edges are added between pairs of vertices with
-probability `p`.
+probability `p`. 
+
+Note that there exists another definition of the Erdös-Rényi model in which the
+total number of edges is kept constant, rather than the probability `p`.
+To access this definition, use `erdos_renyi(n, ne::Integer)`
+(specifically: `erdos_renyi(n, 1) != erdos_renyi(n, 1.0)`).
 
 ### Optional Arguments
 - `is_directed=false`: if true, return a directed graph.
