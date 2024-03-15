e773bce58e206789f7315d0de6c430bffc76f282
Filippos Christou <filchristou@outlook.com>
2024-03-15 08:43:59 +0100

    Bringing the `.github/workflows/postbench.yml` to master (#350)
    * First try on benchmark CI (#315)
    
    (WIP)
    TODO: store results and PR label triggering
    
    * Test consecutive PR (#346)
    
    Merge all new advancements on the `benchx` branch in order to do a follow up consecutive PR to test github actions
    
    * Trivial change, test PR (#347)
    
    * Trivial change, test PR
    
    * Add PR write permission
    
    * Test full permissions
    
    * Granular permissions
    
    * try write-all
    
    * try label event
    
    * labeled in pull_request
    
    * Store results as artifact and trigger a consecutive workflow_run
    
    * Workflow chain almost complete (WIP)
    
    * correct .benchmarkci path
    
    * Add benchx support branch
    
    * Just try pull_request_target instead
    
    * Remove target
    
    * Get rid of s to get the chain going
    
    ---------
    
    Co-authored-by: Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
    
    * Simplify for master merge
    
    * Fixed formatting
    
    * Integrate review comments
    
    ---------
    
    Co-authored-by: Guillaume Dalle <22795598+gdalle@users.noreply.github.com>

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,18 +1,34 @@
-using BenchmarkTools
-using Graphs
+using BenchmarkTools, Graphs
 
-DIGRAPHS = Dict{String,DiGraph}(
+const BENCHDIR = dirname(@__FILE__)
+
+const DIGRAPHS = Dict{String,DiGraph}(
     "complete100" => complete_digraph(100), "path500" => path_digraph(500)
 )
 
-GRAPHS = Dict{String,Graph}(
+const GRAPHS = Dict{String,Graph}(
     "complete100" => complete_graph(100),
     "tutte" => smallgraph(:tutte),
     "path500" => path_graph(500),
 )
 
-suite = BenchmarkGroup()
-include("core.jl")
+serialbenchmarks = [
+    "serial/core.jl",
+    "serial/connectivity.jl",
+    "serial/centrality.jl",
+    "serial/edges.jl",
+    "serial/insertions.jl",
+    "serial/traversals.jl",
+]
 
-tune!(suite);
-results = run(suite; verbose=true, seconds=10)
+const SUITE = BenchmarkGroup()
+
+foreach(serialbenchmarks) do bm
+    include(bm)
+end
+
+parallelbenchmarks = ["parallel/egonets.jl"]
+
+foreach(parallelbenchmarks) do bm
+    include(joinpath(BENCHDIR, bm))
+end
