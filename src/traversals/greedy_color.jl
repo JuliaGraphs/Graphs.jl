77a4dfb095f09244cca5ca8059252e83aa6cd1a6
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-05-27 10:26:58 +0200

    Update src/traversals/greedy_color.jl

--- src/traversals/greedy_color.jl
+++ src/traversals/greedy_color.jl
@@ -23,7 +23,7 @@ function perm_greedy_color(g::AbstractGraph, seq::Vector{T}) where {T<:Integer}
 
     has_self_loops(g) && throw(ArgumentError("graph must not have self loops"))
 
-    colors_used = BitSet()
+    colors_used = Set{T}()
 
     for v in seq
         empty!(colors_used)
