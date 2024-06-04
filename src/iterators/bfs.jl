2224594b88284774a44ac2cce4973ada7887ced1
Tortar <68152031+Tortar@users.noreply.github.com>
2024-06-04 03:25:16 +0200

    Fix bfs iterator for multiple source nodes

--- src/iterators/bfs.jl
+++ src/iterators/bfs.jl
@@ -42,8 +42,10 @@ in a `BitVector` so that to skip those nodes.
 """
 mutable struct BFSVertexIteratorState
     visited::BitVector
-    queue::Vector{Int}
-    neighbor_idx::Int
+    added::BitVector
+    curr_level::Vector{Int}
+    next_level::Vector{Int}
+    node_idx::Int
     n_visited::Int
 end
 
@@ -56,16 +58,17 @@ Base.eltype(::Type{BFSIterator{S,G}}) where {S,G} = eltype(G)
 First iteration to visit vertices in a graph using breadth-first search.
 """
 function Base.iterate(t::BFSIterator{<:Integer})
-    visited = falses(nv(t.graph))
-    visited[t.source] = true
-    return (t.source, BFSVertexIteratorState(visited, [t.source], 1, 1))
+    visited, added = falses(nv(t.graph)), falses(nv(t.graph))
+    visited[t.source] = false
+    state = BFSVertexIteratorState(visited, added, [t.source], Int[], 0, 0)
+    return Base.iterate(t, state)
 end
 
 function Base.iterate(t::BFSIterator{<:AbstractArray})
-    visited = falses(nv(t.graph))
-    visited[first(t.source)] = true
-    state = BFSVertexIteratorState(visited, copy(t.source), 1, 1)
-    return (first(t.source), state)
+    visited, added = falses(nv(t.graph)), falses(nv(t.graph))
+    visited[first(t.source)] = false
+    state = BFSVertexIteratorState(visited, added, copy(t.source), Int[], 0, 0)
+    return Base.iterate(t, state)
 end
 
 """
@@ -74,36 +77,29 @@ end
 Iterator to visit vertices in a graph using breadth-first search.
 """
 function Base.iterate(t::BFSIterator, state::BFSVertexIteratorState)
-    graph, visited, queue = t.graph, state.visited, state.queue
-    while !isempty(queue)
-        if state.n_visited == nv(graph)
-            return nothing
-        end
-        # we visit the first node in the queue
-        node_start = first(queue)
-        if !visited[node_start]
-            visited[node_start] = true
-            state.n_visited += 1
-            return (node_start, state)
-        end
-        # which means we arrive here when the first node was visited.
-        neigh = outneighbors(graph, node_start)
-        if state.neighbor_idx <= length(neigh)
-            node = neigh[state.neighbor_idx]
-            # we update the idx of the neighbor we will visit,
-            # if it is already visited, we repeat
-            state.neighbor_idx += 1
-            if !visited[node]
-                push!(queue, node)
-                state.visited[node] = true
-                state.n_visited += 1
-                return (node, state)
+    state.n_visited == nv(t.graph) && return nothing
+    # we fill nodes in this level
+    if state.node_idx == length(state.curr_level)
+        @inbounds for node in state.curr_level
+            for adj_node in outneighbors(t.graph, node)
+                if !state.visited[adj_node] && !state.added[adj_node]
+                    push!(state.next_level, adj_node)
+                    state.added[adj_node] = true
+                end
             end
-        else
-            # when the first node and its neighbors are visited
-            # we remove the first node of the queue
-            popfirst!(queue)
-            state.neighbor_idx = 1
+        end
+        state.curr_level, state.next_level = state.next_level, Int[]
+        state.node_idx = 0
+    end
+    # we visit all nodes in this level
+    @inbounds while state.node_idx < length(state.curr_level)
+        state.node_idx += 1
+        node = state.curr_level[state.node_idx]
+        if !state.visited[node]
+            state.visited[node] = true
+            state.n_visited += 1
+            return (node, state)
         end
     end
+    return nothing
 end
