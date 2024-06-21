c020f26686b48a1e595b41a76042a09a84f7e5ab
Tortar <68152031+Tortar@users.noreply.github.com>
2024-06-21 19:26:08 +0200

    sort is applicable

--- src/iterators/bfs.jl
+++ src/iterators/bfs.jl
@@ -91,6 +91,7 @@ function Base.iterate(t::BFSIterator, state::BFSVertexIteratorState)
         end
         length(state.next_level) == 0 && return nothing
         state.curr_level, state.next_level = state.next_level, empty!(state.curr_level)
+        sort!(state.curr_level)
         state.node_idx = 0
     end
     # we visit all nodes in this level
