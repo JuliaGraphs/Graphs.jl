9a7a6df10a34a7a7d02c46f38d7eeaa8b9cdea67
Cody Tapscott <84105208+topolarity@users.noreply.github.com>
2025-10-17 17:33:43 -0400

    Add `GraphsSharedArraysExt` and remove `Distributed` dependency (#430)
    * Add `GraphsSharedArraysExt` and remove `Distributed` dependency
    
    * Default to `parallel=:threads` everywhere

--- src/Parallel/traversals/greedy_color.jl
+++ src/Parallel/traversals/greedy_color.jl
@@ -1,5 +1,5 @@
 function random_greedy_color(
-    g::AbstractGraph{T}, reps::Integer; parallel::Symbol=:distributed
+    g::AbstractGraph{T}, reps::Integer; parallel::Symbol=:threads
 ) where {T<:Integer}
     return if parallel === :threads
         threaded_random_greedy_color(g, reps)
@@ -25,20 +25,14 @@ function threaded_random_greedy_color(g::AbstractGraph{T}, reps::Integer) where 
     return convert(Graphs.Coloring{T}, best)
 end
 
-function distr_random_greedy_color(g::AbstractGraph{T}, reps::Integer) where {T<:Integer}
-    best = @distributed (Graphs.best_color) for i in 1:reps
-        seq = shuffle(vertices(g))
-        Graphs.perm_greedy_color(g, seq)
-    end
-
-    return convert(Graphs.Coloring{T}, best)
+function distr_random_greedy_color(args...; kwargs...)
+    return error(
+        "`parallel = :distributed` requested, but SharedArrays or Distributed is not loaded"
+    )
 end
 
 function greedy_color(
-    g::AbstractGraph{U};
-    sort_degree::Bool=false,
-    reps::Integer=1,
-    parallel::Symbol=:distributed,
+    g::AbstractGraph{U}; sort_degree::Bool=false, reps::Integer=1, parallel::Symbol=:threads
 ) where {U<:Integer}
     return if sort_degree
         Graphs.degree_greedy_color(g)
