4abcddca1e49c9e740b98383c6442df42b1ce8eb
matbesancon <mathieu.besancon@gmail.com>
2026-01-26 16:59:25 +0100

    accept spanning tree vector instead of matrix for Kruskal

--- test/spanningtrees/kruskal.jl
+++ test/spanningtrees/kruskal.jl
@@ -8,6 +8,8 @@
         6 10 3 0
     ]
 
+    weight_vector = [distmx[src(e), dst(e)] for e in edges(g4)]
+
     vec_mst = Vector{Edge}([Edge(1, 2), Edge(3, 4), Edge(2, 3)])
     max_vec_mst = Vector{Edge}([Edge(2, 4), Edge(1, 4), Edge(1, 3)])
     for g in test_generic_graphs(g4)
@@ -18,7 +20,13 @@
         # so instead we compare tuples of source and target vertices
         @test sort([(src(e), dst(e)) for e in mst]) == sort([(src(e), dst(e)) for e in vec_mst])
         @test sort([(src(e), dst(e)) for e in max_mst]) == sort([(src(e), dst(e)) for e in max_vec_mst])
+        # test equivalent vector form
+        mst_vec = @inferred(kruskal_mst(g, weight_vector))
+        max_mst_vec = @inferred(kruskal_mst(g, weight_vector, minimize=false))
+        @test mst_vec == mst
+        @test max_mst_vec == max_mst        
     end
+
     # second test
     distmx_sec = [
         0 0 0.26 0 0.38 0 0.58 0.16
@@ -32,6 +40,8 @@
     ]
 
     gx = SimpleGraph(distmx_sec)
+    weight_vector_sec = [distmx_sec[src(e), dst(e)] for e in edges(gx)]
+
     vec2 = Vector{Edge}([
         Edge(1, 8), Edge(3, 4), Edge(2, 8), Edge(1, 3), Edge(6, 8), Edge(5, 6), Edge(3, 7)
     ])
@@ -40,9 +50,13 @@
     ])
     for g in test_generic_graphs(gx)
         mst2 = @inferred(kruskal_mst(g, distmx_sec))
+        mst2_vec = @inferred(kruskal_mst(g, weight_vector_sec))
         max_mst2 = @inferred(kruskal_mst(g, distmx_sec, minimize=false))
+        max_mst2_vec = @inferred(kruskal_mst(g, weight_vector_sec, minimize=false))
         @test sort([(src(e), dst(e)) for e in mst2]) == sort([(src(e), dst(e)) for e in vec2])
         @test sort([(src(e), dst(e)) for e in max_mst2]) == sort([(src(e), dst(e)) for e in max_vec2])
+        @test mst2 == mst2_vec
+        @test max_mst2 == max_mst2_vec
     end
 
     # non regression test for #362
